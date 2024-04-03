433b0d2eb1349e0b0c58daba8cc551c2b3e63810
njlyon0 <nicholasjlyon@gmail.com>
2024-04-03 10:20:38 -0400

    Fleshed out raster prep and added CRS check/transformation for vector data prep

--- dev/prep_spatial.R
+++ dev/prep_spatial.R
@@ -29,11 +29,14 @@ sf_file <- system.file(file.path("shape", "nc.shp"), package = "sf")
 # Read it in
 sf_obj <- sf::st_read(dsn = sf_file)
 
+# Transform CRS
+sf_wgs84 <- sf::st_transform(x = sf_obj, crs = 4326)
+
 # Exploratory plot
-plot(sf_obj, axes = T)
+plot(sf_wgs84["AREA"], axes = T)
 
 # Export locally to a more easily accessible directory
-sf::st_write(obj = sf_obj, dsn = file.path("data", "nc.shp"), delete_layer = T)
+sf::st_write(obj = sf_wgs84, dsn = file.path("data", "nc.shp"), delete_layer = T)
 
 ## ----------------------- ##
   # Raster Data Prep ----
@@ -51,10 +54,10 @@ sf::st_write(obj = sf_obj, dsn = file.path("data", "nc.shp"), delete_layer = T)
 rast_raw <- terra::rast(x = file.path("data", "SRTMGL3_NC.003_SRTMGL3_DEM_doy2000042_aid0001.tif"))
 
 # Crop raster to polygon above
-rast_crop <- terra::crop(x = rast_raw, y = terra::vect(sf_obj["AREA"]), mask = T)
+rast_crop <- terra::crop(x = rast_raw, y = terra::vect(sf_wgs84["AREA"]), mask = T)
 
 # Test plot
-plot(rast_crop, axes = T)
+terra::plot(rast_crop)
 
 # Export locally
 terra::writeRaster(x = rast_crop, overwrite = T,

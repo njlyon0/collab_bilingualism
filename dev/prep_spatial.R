c36d97a6c780648cfccb318633b04d1a61cc9010
njlyon0 <nicholasjlyon@gmail.com>
2024-04-03 09:56:11 -0400

    Preparing vector data for spatial modules

--- dev/prep_spatial.R
+++ dev/prep_spatial.R
@@ -13,8 +13,29 @@
 # Load needed libraries
 librarian::shelf(sf)
 
+# Create the 'data' folder if it doesn't exist
+dir.create(path = file.path("data"), showWarnings = F)
+
 # Clear environment
 rm(list = ls())
+
+## ----------------------- ##
+  # Vector Data Prep ----
+## ----------------------- ##
+
+# Identify path to `sf` North Carolina vector data
+sf_file <- system.file(file.path("shape", "nc.shp"), package = "sf")
+
+# Read it in
+sf_obj <- sf::st_read(dsn = sf_file)
+
+# Exploratory plot
+plot(sf_obj, axes = T)
+
+# Export locally to a more easily accessible directory
+sf::st_write(obj = sf_obj, dsn = file.path("data", "nc.shp"), delete_layer = T)
+
+
 
 
 # End ----

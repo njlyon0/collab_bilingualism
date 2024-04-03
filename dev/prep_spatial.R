a08de85c7986f6e8fd2e02c3eec70169070484be
njlyon0 <nicholasjlyon@gmail.com>
2024-04-03 09:50:46 -0400

    Fleshed out spatial prep script skeleton

---
+++ dev/prep_spatial.R
@@ -0,0 +1,20 @@
+## -------------------------------------- ##
+        # Spatial Module Prep Work
+## -------------------------------------- ##
+# Author(s): Nick J Lyon
+
+# Purpose:
+## Prepare data for the spatial modules
+
+## ----------------------- ##
+    # Housekeeping ----
+## ----------------------- ##
+
+# Load needed libraries
+librarian::shelf(sf)
+
+# Clear environment
+rm(list = ls())
+
+
+# End ----
